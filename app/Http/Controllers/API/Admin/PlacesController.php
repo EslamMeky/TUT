c1a6ef10cd6fd97bc227a5e21cebb20d9a6cea1f
EsLaM MeKky <eslammeky111@gmail.com>
2024-04-22 19:53:51 +0200

    photo places

--- app/Http/Controllers/API/Admin/PlacesController.php
+++ app/Http/Controllers/API/Admin/PlacesController.php
@@ -59,7 +59,7 @@ class PlacesController extends Controller
     {
         try
         {
-            $place=Place::with(['cities'])->selection()->orderBy('id','desc')->paginate(PAGINATE);
+            $place=Place::with(['cities'])->selection()->orderBy('id','asc')->paginate(PAGINATE);
             return $this->ReturnData('Places',$place,'Done');
         }
         catch (\Exception $ex){
