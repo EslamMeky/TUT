bd64ed360c3098ef40b03d16ec627623d33ae2cc
RitaRawhy <rita.2000489@gmail.com>
2024-05-10 16:19:25 +0300

    add recommendation model

---
+++ app/Models/Recommendation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recommendation extends Model
+{
+    use HasFactory;
+    protected $table='recommendations';
+    protected $fillable=[
+        'id',
+        'user_id',
+        'place_id',
+        'created_at',
+        'updated_at'
+    ];
+    public $timestamps=true;
+
+    public function users()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+
+    public function places()
+    {
+        return  $this->belongsTo(Place::class,'place_id','id');
+    }
+}
